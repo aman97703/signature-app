c28778b6cbb383449eb91f5724a93bffd87e6d17
Aman Gupta <aman97703@gmail.com>
2024-09-07 17:26:50 +0530

    update

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,7 @@ import Board from "@/components/board";
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center p-12">
-      <h1 className="text-4xl font-bold mb-8">Whiteboard</h1>
+      <h1 className="text-4xl font-bold mb-8">Sign Here</h1>
       <Board/>
     </main>
   );
